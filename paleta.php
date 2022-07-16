2bb55edb0b306f07466c78fceb51e89dacae2101
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2022-07-15 21:47:38 -0300

    Create paleta.php

---
+++ paleta.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nome = "Matheus";
+    echo "meu nome é $nome";
+
+    echo "<br>";
+    echo " Meu primeiro código php no gthub";
+    
+  if(is_string($nome))){
+    echo 
+
+  }
+?>
